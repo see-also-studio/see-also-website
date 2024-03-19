229ab8d4d3fe60fca846a31ef0547854a91e046a
m-oo <31993758+m-oo@users.noreply.github.com>
2024-03-19 15:31:51 +0000

    Update weather.js
    - Add temperature to output string
    - Update weather every 10 minutes

--- src/scripts/components/weather.js
+++ src/scripts/components/weather.js
@@ -1,14 +1,19 @@
 export class CurrentWeather {
   constructor(el) {
+    const key = 'ea2b78fb74b05873' + 'c8d64ca1e69abe37';
     this.el = el;
-    this.updateWeather('ea2b78fb74b05873' + 'c8d64ca1e69abe37');
+    this.updateWeather(key);
+    setInterval(() => {
+      console.info('Getting weather update');
+      this.updateWeather(key);
+    }, 600000);
   }
 
   updateWeather(key) {
     const url = 'https://api.openweathermap.org/data/2.5/weather?id=2643741&units=metric&appid=' + key;
     fetch(url)
     .then(data => data.json())
-    .then(data => this.el.innerText = data['weather'][0]['description'] + ' in the city')
+    .then(data => this.el.innerText = `${data['weather'][0]['description']} in the city, ${Math.round(data['main']['temp'])}°`)
     .catch(() => console.log('current weather api request failed.'));
   }
 }
