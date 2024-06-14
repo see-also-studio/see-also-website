89f09bebfdf2d87bfa0a0d5f4de779715b50bc62
m-oo <31993758+m-oo@users.noreply.github.com>
2024-06-14 13:23:29 +0100

    Update orbs
    - Offset orbs slightly from edge
    - Reduce blur of orb slightly as visual illusion makes it looked wrong

--- src/scripts/components/orb.js
+++ src/scripts/components/orb.js
@@ -1,10 +1,10 @@
-import { randomFromRange } from "../functions/random";
+import { randomFromRange, randomFromArray } from "../functions/random";
 
 export function orbPosition(el) {
   console.log('Randomizing orbs');
   const orientation = randomFromRange(0, 1); // 0 = left/right, 1 = top/bottom
-  const x = orientation ? randomFromRange(0, 1) * 100 : randomFromRange(0, 100);
-  const y = orientation ? randomFromRange(0, 100) : randomFromRange(0, 1) * 100;
-  el.style.setProperty('--left', x + '%');
-  el.style.setProperty('--top', y + '%');
+  const x = orientation ? `${randomFromArray(['calc(0% - 1em)', 'calc(100% + 1em)'])}` : `${randomFromRange(0, 100)}%`;
+  const y = orientation ? `${randomFromRange(0, 100)}%` : `${randomFromArray(['calc(0% - 1em)', 'calc(100% + 1em)'])}`;
+  el.style.setProperty('--left', x);
+  el.style.setProperty('--top', y);
 }
--- src/scripts/functions/random.js
+++ src/scripts/functions/random.js
@@ -3,3 +3,8 @@ export function randomFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// return random from array
+export function randomFromArray(array) {
+  return array[randomFromRange(0, array.length - 1)];
+}
+
