57c31712b202c2c4ed43446a35737c30a58a2e11
m-oo <31993758+m-oo@users.noreply.github.com>
2024-06-14 19:34:19 +0100

    Update orbs positioning
    - Make orbs position more randomly off from the gallery area
    - Rework randomFromRange function to take decimalPlaces argument
    - Adjust some zIndexes so that orbs appear correctly below buttons and text

--- src/scripts/components/orb.js
+++ src/scripts/components/orb.js
@@ -3,8 +3,12 @@ import { randomFromRange, randomFromArray } from "../functions/random";
 export function orbPosition(el) {
   console.log('Randomizing orbs');
   const orientation = randomFromRange(0, 1); // 0 = left/right, 1 = top/bottom
-  const x = orientation ? `${randomFromArray(['calc(0% - 1em)', 'calc(100% + 1em)'])}` : `${randomFromRange(0, 100)}%`;
-  const y = orientation ? `${randomFromRange(0, 100)}%` : `${randomFromArray(['calc(0% - 1em)', 'calc(100% + 1em)'])}`;
+  const x = orientation 
+    ? `${randomFromArray([`calc(0% - ${randomFromRange(1, 5, 1)}em)`, `calc(100% + ${randomFromRange(1, 5, 1)}em)`])}`
+    : `${randomFromRange(0, 50)}%`;
+  const y = orientation 
+    ? `${randomFromRange(0, 100)}%` 
+    : `${randomFromArray([`calc(0% - ${randomFromRange(1, 5, 1)}em)`, `calc(100% + ${randomFromRange(1, 5, 1)}em)`])}`;
   el.style.setProperty('--left', x);
   el.style.setProperty('--top', y);
 }
--- src/scripts/functions/random.js
+++ src/scripts/functions/random.js
@@ -1,6 +1,14 @@
 // return random number between min and max
-export function randomFromRange(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+export function randomFromRange(min, max, decimalPlaces = 0) {
+  if (min >= max) {
+    throw new Error('The value of min must be less than the value of max.');
+  }
+  if (decimalPlaces < 0) {
+    throw new Error('decimalPlaces must be a positive integer.');
+  }
+
+  const random = Math.random() * (max - min) + min;
+  return parseFloat(random.toFixed(decimalPlaces));
 }
 
 // return random from array
