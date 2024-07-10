e8fad070b45523586f565c0a4313609cddcdb240
m-oo <31993758+m-oo@users.noreply.github.com>
2024-07-10 18:08:27 +0100

    Update orb scatter area to be further from image
    - Increase minimum distance that orb can be to image gallery from 1em to 1.7em

--- src/scripts/components/orb.js
+++ src/scripts/components/orb.js
@@ -4,11 +4,11 @@ export function orbPosition(el) {
   console.log('Randomizing orbs');
   const orientation = randomFromRange(0, 1); // 0 = left/right, 1 = top/bottom
   const x = orientation 
-    ? `${randomFromArray([`calc(0% - ${randomFromRange(1, 5, 1)}em)`, `calc(100% + ${randomFromRange(1, 5, 1)}em)`])}`
+    ? `${randomFromArray([`calc(0% - ${randomFromRange(1.7, 5, 1)}em)`, `calc(100% + ${randomFromRange(1.7, 5, 1)}em)`])}`
     : `${randomFromRange(0, 50)}%`;
   const y = orientation 
     ? `${randomFromRange(0, 100)}%` 
-    : `${randomFromArray([`calc(0% - ${randomFromRange(1, 5, 1)}em)`, `calc(100% + ${randomFromRange(1, 5, 1)}em)`])}`;
+    : `${randomFromArray([`calc(0% - ${randomFromRange(1.7, 5, 1)}em)`, `calc(100% + ${randomFromRange(1.7, 5, 1)}em)`])}`;
   el.style.setProperty('--left', x);
   el.style.setProperty('--top', y);
 }
